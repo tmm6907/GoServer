3a2f3d9579f6a5ddf51360eaaa1c21fdb3614837
Terrence <tmm6907@gmail.com>
2023-06-19 12:04:53 -0400

    Add RapidAPI proxy auth

--- src/nwi.go
+++ src/nwi.go
@@ -176,10 +176,10 @@ func main() {
 	}
 	// "/cloudsql/"+connectionName,
 	dbUrl := fmt.Sprintf(
-		"%s:%s@tcp(%s)/%s?parseTime=true",
+		"%s:%s@unix(%s)/%s?parseTime=true",
 		dbUser,
 		dbPass,
-		"localhost",
+		"/cloudsql/"+connectionName,
 		dbName,
 	)
 	db, err := init_db(dbUrl)
